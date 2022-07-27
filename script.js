27eb1124b00a0449587bace4b37afb6273c1bf7f
duckyrshannon <duckyrshannon@gmail.com>
2022-07-27 15:59:10 -0700

    add clear and allow multiple calculations

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ let division = document.querySelector(".divide");
 let numberBtn = document.querySelectorAll(".number");
 let operatorBtn = document.querySelectorAll(".operator");
 let computeBtn = document.querySelector(".equals");
-let userInput = document.querySelector(".calcScreen");
 let calcResult = document.querySelector(".resultScreen");
+let clearBtn = document.querySelector(".clear");
 
 let numA = [];
 let numB = [];
 let operator;
 let result;
+let sign;
+let displayValue;
+let displayValueB;
 
 numberBtn.forEach(function (button) {
   button.addEventListener("click", function () {
@@ -19,14 +22,18 @@ numberBtn.forEach(function (button) {
       if (numA.length < 4) {
         //want max length userNum = 4
         numA.push(button.textContent);
+        displayValue = numA.join("");
+        calcResult.textContent = displayValue;
       }
       console.log(`numA = ${numA}`); //join later
     } else if (typeof operator !== "undefined") {
       if (numB.length < 4) {
         //want max length userNum = 4
         numB.push(button.textContent);
+        displayValueB = numB.join("");
       }
       console.log(`numB = ${numB}`); //join later
+      calcResult.textContent = `${displayValue} ${sign} ${displayValueB}`;
     }
   });
 });
@@ -36,21 +43,52 @@ getOperator();
 function getOperator() {
   operatorBtn.forEach(function (button) {
     button.addEventListener("click", function () {
+      if (operator !== undefined) {
+        calculate();
+      }
       if (button.textContent == "+") {
+        calcResult.textContent = `${displayValue} +`;
         operator = "addition";
+        sign = "+";
       } else if (button.textContent == "-") {
+        calcResult.textContent = `${displayValue} -`;
         operator = "subtraction";
+        sign = "-";
       } else if (button.textContent == "x") {
+        calcResult.textContent = `${displayValue} x`;
         operator = "multiplication";
+        sign = "x";
       } else if (button.textContent == "/") {
+        calcResult.textContent = `${displayValue} /`;
         operator = "division";
+        sign = "/";
       }
       return console.log(operator);
     });
   });
 }
 
-computeBtn.addEventListener("click", function operate() {
+computeBtn.addEventListener("click", calculate);
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+clearBtn.addEventListener("click", function () {
+  reset();
+  result = [];
+  numA = [];
+  displayValue = [];
+  calcResult.textContent = "";
+});
+
+function reset() {
+  numB = [];
+  operator = undefined;
+}
+
+function calculate() {
   if (typeof numA == "object") {
     numA = numA.join("");
   }
@@ -75,11 +113,7 @@ computeBtn.addEventListener("click", function operate() {
   }
   calcResult.textContent = result;
   numA = result;
-  numB = [];
+  displayValue = result;
+  reset();
   return console.log(result);
-});
-
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
+}
